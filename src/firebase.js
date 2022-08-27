7d15c188e78b4240d9b34d2792d545f9419050f2
Jeffrey Gil P. Deimos <jeffroxgil@gmail.com>
2022-08-27 17:54:53 +0800

    Create and select channels using firebase.

---
+++ src/firebase.js
@@ -0,0 +1,21 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyAKKubzjp6rlGfDMT0tw_lYbEyi-xcnmhU",
+    authDomain: "react-slack-403ea.firebaseapp.com",
+    projectId: "react-slack-403ea",
+    storageBucket: "react-slack-403ea.appspot.com",
+    messagingSenderId: "400074732951",
+    appId: "1:400074732951:web:529897fdcfa6cd2e8bc0d2"
+  };
+
+  // Use this to initialize the firebase App
+  const firebaseapp = firebase.initializeApp(firebaseConfig);
+
+  // Use these for db, auth & provider
+  const db = firebaseapp.firestore();
+
+  export { db }
